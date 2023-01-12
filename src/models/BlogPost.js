27b57a8e143e9ea98d8e0aeabc448c05f6388764
Caio Vinícius <caiooom91@gmail.com>
2023-01-12 11:52:38 -0300

    Atualiza o model BlogPost

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -1,8 +1,45 @@
-module.exports = (sequelize, _DataTypes) => {
-  const BlogPost = sequelize.define('BlogPost', {});
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost',
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+      },
+      title: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      published: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      timestamps: true,
+      createdAt: 'published',
+      updatedAt: 'updated',
+      tableName: 'blog_posts',
+      underscored: true,
+    }
+  );
+
   BlogPost.associate = (models) => {
     BlogPost.belongsTo(models.User,
       { foreignKey: 'userId', as: 'users' });
   };
+
   return BlogPost;
 } 
